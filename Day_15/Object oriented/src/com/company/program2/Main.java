2bfc311aaeb54e4049f4a3446b853f120c425f7b
Jason <jason.onggo@tempatkerja.com>
2020-10-16 17:20:11 +0700

    add Day 15

--- Day_15/Object oriented/src/com/company/program2/Main.java
+++ Day_15/Object oriented/src/com/company/program2/Main.java
@@ -1,15 +1,16 @@
 package com.company.program2;
 
+import com.company.program2.util.LoginUtil;
 import com.company.program2.util.MenuUtil;
 
 public class Main {
 
   public static void main(String[] args) {
-//    LoginUtil loginUtil = new loginUtil();
-//
-//    if (!loginUtil.isAuthState()) {
-//      return;
-//    }
+   LoginUtil loginUtil = new LoginUtil();
+
+   if (!loginUtil.isAuthState()) {
+     return;
+   }
 
     MenuUtil menuUtil = new MenuUtil();
     menuUtil.showMenu();
