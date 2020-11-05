9ebe4752cac4162c7131a3f6462d4c56c7e448e8
Jason <jason.onggo@tempatkerja.com>
2020-11-05 21:32:02 +0700

    remove unused import

--- Bank_Account/reactjs/src/components/home/home.tsx
+++ Bank_Account/reactjs/src/components/home/home.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
-import { idText } from "typescript";
 import { logout } from "../../features/authorization/authorizationSlice";
 import styles from "./home.module.css";
 
