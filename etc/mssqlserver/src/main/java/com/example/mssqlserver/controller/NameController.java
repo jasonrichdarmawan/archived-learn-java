f28dd21d15065cda9ad264668e0d8c7070a511f8
Jason <jason.onggo@tempatkerja.com>
2020-11-17 23:02:46 +0700

    remove unncessary if statemnt

--- etc/mssqlserver/src/main/java/com/example/mssqlserver/controller/NameController.java
+++ etc/mssqlserver/src/main/java/com/example/mssqlserver/controller/NameController.java
@@ -5,7 +5,10 @@ import com.example.mssqlserver.model.NameModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
@@ -17,15 +20,11 @@ public class NameController {
 
   @GetMapping("api/v1/name")
   public ResponseEntity<?> selectAll() {
-    return new ResponseEntity<>(nameMapper.selectAll(), HttpStatus.OK);
+    return ResponseEntity.ok().body(nameMapper.selectAll());
   }
 
   @PostMapping("api/v1/name")
   public ResponseEntity<?> insert(@Valid @RequestBody NameModel nameModel) {
-    if (nameMapper.insert(nameModel) == 1) {
-      return ResponseEntity.ok().body(nameModel);
-    } else {
-      return ResponseEntity.badRequest().build();
-    }
+    return ResponseEntity.ok().body(nameModel);
   }
 }
