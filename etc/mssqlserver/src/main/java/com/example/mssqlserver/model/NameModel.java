1a0b09bcec8f8608dc21511ef2026ba6d8265c43
Jason <jason.onggo@tempatkerja.com>
2020-11-17 21:51:42 +0700

    remove unused code

--- etc/mssqlserver/src/main/java/com/example/mssqlserver/model/NameModel.java
+++ etc/mssqlserver/src/main/java/com/example/mssqlserver/model/NameModel.java
@@ -8,18 +8,8 @@ public class NameModel {
   private String name;
   private String newid;
 
-  public NameModel(Integer id, String name, String newid) {
-    this.id = id;
-    this.name = name;
-    this.newid = newid;
-  }
-
   public Integer getId() {
     return id;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
   }
 
   public String getName() {
@@ -28,10 +18,6 @@ public class NameModel {
 
   public String getNewid() {
     return newid;
-  }
-
-  public void setNewid(String newid) {
-    this.newid = newid;
   }
 
   public boolean requestIsValid() {
