4b986d305eac7454854ab693573f6f08e0987b8a
Jason <jason.onggo@tempatkerja.com>
2020-11-17 23:08:35 +0700

    remove unused import

--- etc/mssqlserver/src/main/java/com/example/mssqlserver/controller/NameController.java
+++ etc/mssqlserver/src/main/java/com/example/mssqlserver/controller/NameController.java
@@ -3,7 +3,6 @@ package com.example.mssqlserver.controller;
 import com.example.mssqlserver.mapper.NameMapper;
 import com.example.mssqlserver.model.NameModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
-public class NameController {
+public class gNameController {
 
   @Autowired
   private NameMapper nameMapper;
