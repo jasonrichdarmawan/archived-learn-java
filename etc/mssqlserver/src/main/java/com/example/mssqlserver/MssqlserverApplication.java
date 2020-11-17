800688b330718e3d080260cd3db251fc048da242
Jason <jason.onggo@tempatkerja.com>
2020-11-17 09:30:52 +0700

    remove unused annotation

--- etc/mssqlserver/src/main/java/com/example/mssqlserver/MssqlserverApplication.java
+++ etc/mssqlserver/src/main/java/com/example/mssqlserver/MssqlserverApplication.java
@@ -1,13 +1,8 @@
 package com.example.mssqlserver;
 
-import com.example.mssqlserver.model.NameModel;
-import org.apache.ibatis.type.MappedTypes;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MappedTypes(NameModel.class)
-@MapperScan(basePackages = "com.example.mssqlserver.mapper")
 @SpringBootApplication
 public class MssqlserverApplication {
 
