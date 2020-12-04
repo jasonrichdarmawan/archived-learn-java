a76b40b40aa9d88c9ed6cbee821b730a97ba56b1
Jason <jason.onggo@tempatkerja.com>
2020-12-04 18:45:53 +0700

    update dumps sql and fix be-bankaccount container can't connect to db-mysql

--- etc/bankaccount/dumps/bankaccount/data/db-bankaccount_customer.sql
+++ etc/bankaccount/dumps/bankaccount/data/db-bankaccount_customer.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `db-bankaccount` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db-bankaccount`;
 -- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
 --
 -- Host: localhost    Database: db-bankaccount
