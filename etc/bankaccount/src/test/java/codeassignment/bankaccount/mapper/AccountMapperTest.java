847e7688a80839214d14c9079bb535d267b13bfd
Jason <jason.onggo@tempatkerja.com>
2020-12-05 02:14:25 +0700

    add AccountMapperTest

---
+++ etc/bankaccount/src/test/java/codeassignment/bankaccount/mapper/AccountMapperTest.java
@@ -0,0 +1,28 @@
+package codeassignment.bankaccount.mapper;
+
+import codeassignment.bankaccount.model.PostTransferModel;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class AccountMapperTest {
+
+  @Autowired
+  private AccountMapper accountMapper;
+
+  @Test
+  void updateBalance() {
+    int rowsAffected = accountMapper.updateBalance(new BigDecimal(15000),"555001");
+    BigDecimal balance = accountMapper.getBalance("555001");
+    Assertions.assertThat(rowsAffected).isEqualTo(1);
+    Assertions.assertThat(balance).isEqualByComparingTo(new BigDecimal(15000));
+  }
+}
