9c973b0330a215084f497fe4969afd3493178e07
Jason <jason.onggo@tempatkerja.com>
2020-12-21 15:51:46 +0700

    use @Header(value = Authorization) to get the key value

--- etc/websocket-2/src/main/java/learn/websocket2/controller/WebSocketChatController.java
+++ etc/websocket-2/src/main/java/learn/websocket2/controller/WebSocketChatController.java
@@ -1,9 +1,7 @@
 package learn.websocket2.controller;
 
 import learn.websocket2.domain.WebSocketChatMessage;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.handler.annotation.*;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
@@ -18,7 +16,8 @@ public class WebSocketChatController {
 
   @MessageMapping("/chat.newUser")
   @SendTo("/topic/javainuse")
-  public WebSocketChatMessage newUser(@Payload WebSocketChatMessage message, SimpMessageHeaderAccessor headerAccessor) {
+  public WebSocketChatMessage newUser(@Payload WebSocketChatMessage message, SimpMessageHeaderAccessor headerAccessor, @Header(value = "Authorization") String authorization) {
+    System.out.println("Authorization: " + authorization);
     headerAccessor.getSessionAttributes().put("username", message.getSender()); // dependency of WebSocketChatEventListener to handle SessionDisconnectEvent.
     return message;
   }
