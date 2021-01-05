14f2aefd3ebf17c552fe4028719d3914355a5b18
Jason <jason.onggo@tempatkerja.com>
2021-01-05 20:47:26 +0700

    fix bug

---
+++ etc/authorization-server/README.md
@@ -0,0 +1,4 @@
+Usage
+```
+curl localhost:9000/oauth2/token --user pilot-client:secret -dgrant_type=client_credentials
+```
--- etc/authorization-server/src/main/java/com/example/authorizationserver/AuthorizationServerConfig.java
+++ etc/authorization-server/src/main/java/com/example/authorizationserver/AuthorizationServerConfig.java
@@ -2,8 +2,12 @@ package com.example.authorizationserver;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.crypto.keys.KeyManager;
 import org.springframework.security.crypto.keys.StaticKeyGeneratingKeyManager;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
@@ -11,12 +15,43 @@ import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import java.util.UUID;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 @EnableWebSecurity
+@Order(101)
 @Import(OAuth2AuthorizationServerConfiguration.class)
-public class AuthorizationServerConfig {
+public class AuthorizationServerConfig
+        // fix issue option 1:
+        // Found WebSecurityConfigurerAdapter as well as SecurityFilterChain. Please select just one.
+        // alternative @Bean SecurityFilterChain
+        extends WebSecurityConfigurerAdapter
+{
+
+//  // fix issue option 2:
+//  @Bean
+//  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//    OAuth2AuthorizationServerConfigurer<HttpSecurity> authorizationServerConfigurer =
+//            new OAuth2AuthorizationServerConfigurer<>();
+//    RequestMatcher[] endpointMatchers = authorizationServerConfigurer
+//            .getEndpointMatchers().toArray(new RequestMatcher[0]);
+//
+//    httpSecurity
+//            .requestMatcher(new OrRequestMatcher(endpointMatchers))
+//            .authorizeRequests(authorizeRequests ->
+//                    authorizeRequests.anyRequest().authenticated()
+//            )
+//            .formLogin(withDefaults())
+//            .csrf(csrf -> csrf.ignoringRequestMatchers(endpointMatchers))
+//            .apply(authorizationServerConfigurer);
+//
+//    return httpSecurity.build();
+//  }
 
   @Bean
   public RegisteredClientRepository registeredClientRepository() {
