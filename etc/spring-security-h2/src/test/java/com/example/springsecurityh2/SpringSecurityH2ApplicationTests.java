6dc39659421db4c2c9ce5439966bf440a046a3e6
Jason <jason.onggo@tempatkerja.com>
2021-01-06 10:20:47 +0700

    first commit spring-security-h2

---
+++ etc/spring-security-h2/src/test/java/com/example/springsecurityh2/SpringSecurityH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
