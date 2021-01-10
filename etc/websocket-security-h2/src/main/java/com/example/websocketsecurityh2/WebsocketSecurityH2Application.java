4bcee5736c920c1fa6dcbf2bc4272204f976b5d1
Jason <jason.onggo@tempatkerja.com>
2021-01-10 19:31:36 +0700

    first commit websocket-security-h2

---
+++ etc/websocket-security-h2/src/main/java/com/example/websocketsecurityh2/WebsocketSecurityH2Application.java
@@ -0,0 +1,13 @@
+package com.example.websocketsecurityh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketSecurityH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketSecurityH2Application.class, args);
+	}
+
+}
---
+++ etc/websocket-security-h2/src/test/java/com/example/websocketsecurityh2/WebsocketSecurityH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websocketsecurityh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketSecurityH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
