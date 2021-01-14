3dc0eb33c05038f6ec203a23e06d357b58110aca
Jason <jason.onggo@tempatkerja.com>
2021-01-14 14:32:33 +0700

    add example with Global CORS

--- etc/spring-security-angular/angular/src/app/app.component.ts
+++ etc/spring-security-angular/angular/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http"
 
+interface Greeting {
+  id:string, content: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent{
+  baseURL = "http://localhost:8080";
+  title = 'Demo';
+  greeting: Greeting = { id: "", content: ""};
+  constructor(private http: HttpClient) {
+    this.http.get(`${this.baseURL}/resource`).subscribe((data: Greeting) => this.greeting = data);
+  }
 }
---
+++ etc/spring-security-angular/spring-security/src/main/java/com/example/springsecurity/config/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+            .cors(withDefaults())
+            .authorizeRequests()
+            .antMatchers("/resource").permitAll()
+            .anyRequest().authenticated();
+  }
+
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+}
