9ed4a9651d343552b7dcdcf106877ed6bc84f02c
Jason <jason.onggo@tempatkerja.com>
2021-02-02 16:11:17 +0700

    add To Do

---
+++ etc/websocket-security-h2/src/main/java/com/example/websocketsecurityh2/controller/UserDetailsController.java
@@ -0,0 +1,21 @@
+package com.example.websocketsecurityh2.controller;
+
+import com.example.websocketsecurityh2.model.MyUserDetails;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserDetailsController {
+  // TODO: hide password.
+  @GetMapping("/userdetails")
+  public MyUserDetails getUserDetails(@CurrentSecurityContext(expression = "authentication.principal") Object user) {
+    return (MyUserDetails) user;
+  }
+
+  // TODO: [BUG] CSRF protection.
+  @GetMapping("/helloworld")
+  public String getHelloWorld() {
+    return "Hello World";
+  }
+}
