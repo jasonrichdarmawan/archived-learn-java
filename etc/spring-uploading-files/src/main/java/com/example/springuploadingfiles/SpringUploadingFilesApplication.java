8bb738d3700df60b6241bbf7f621ab8a8de95f65
Jason <jason.onggo@tempatkerja.com>
2021-02-27 23:55:26 +0700

    add example, learn next week

--- etc/spring-uploading-files/src/main/java/com/example/springuploadingfiles/SpringUploadingFilesApplication.java
+++ etc/spring-uploading-files/src/main/java/com/example/springuploadingfiles/SpringUploadingFilesApplication.java
@@ -1,13 +1,26 @@
 package com.example.springuploadingfiles;
 
+import com.example.springuploadingfiles.storage.StorageProperties;
+import com.example.springuploadingfiles.storage.StorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class SpringUploadingFilesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringUploadingFilesApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(StorageService storageService) {
+		return (args) -> {
+			storageService.deleteAll();
+			storageService.init();
+		};
+	}
 }
