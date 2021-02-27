65a7db0afb5cf7d866d4715532e321a072b0db3e
Jason <jason.onggo@tempatkerja.com>
2021-02-27 23:14:55 +0700

    first commit spring-uploading-files

---
+++ etc/spring-uploading-files/src/main/java/com/example/springuploadingfiles/SpringUploadingFilesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springuploadingfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUploadingFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringUploadingFilesApplication.class, args);
+	}
+
+}
---
+++ etc/spring-uploading-files/src/test/java/com/example/springuploadingfiles/SpringUploadingFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springuploadingfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUploadingFilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
